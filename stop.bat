f8eab180e38f33fda3edec4aa06572a27fd6ee98
BaThanhHuynh <huynhbathanh21102005@gmail.com>
2026-07-09 01:29:07 +0700

    final commit

--- stop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Dừng Quản Lý Chi Tiêu (Docker & Cloudflare)
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "stop.ps1"
-echo.
-echo Nhấn phím bất kỳ để đóng cửa sổ này...
-pause > nul
